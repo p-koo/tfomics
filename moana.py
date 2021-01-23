cb77314cfcdf2b9d5e4c769a95fa82adc58c6fb7
koo-lab <koolaboratory.gmail.com>
2021-01-23 00:19:30 -0500

    updates

--- moana.py
+++ moana.py
@@ -219,43 +219,5 @@ def match_hits_to_ground_truth(file_path, motifs, motif_names=None, num_filters=
   return match_fraction, match_any, filter_match, filter_qvalue, motif_qvalue, motif_counts
 
 
-def motif_comparison_synthetic_dataset(file_path, num_filters=32):
-  """ Compares tomtom analysis for filters trained on synthetic multitask classification.
-      Works with Tomtom version 5.1.0 
- 
-  inputs:
-      - file_path: .tsv file output from tomtom analysis
-      - num_filters: number of filters in conv layer (needed to normalize -- tomtom doesn't always give results for every filter)
-
-  outputs:
-      - match_fraction: fraction of hits to ground truth motifs
-      - match_any: fraction of hits to any motif in JASPAR (except Gremb1)
-      - filter_match: the motif of the best hit (to a ground truth motif)
-      - filter_qvalue: the q-value of the best hit to a ground truth motif (1.0 means no hit)
-      - motif_qvalue: for each ground truth motif, gives the best qvalue hit
-      - motif_counts for each ground truth motif, gives number of filter hits
-  """
-
-  arid3 = ['MA0151.1', 'MA0601.1', 'PB0001.1']
-  cebpb = ['MA0466.1', 'MA0466.2']
-  fosl1 = ['MA0477.1']
-  gabpa = ['MA0062.1', 'MA0062.2']
-  mafk = ['MA0496.1', 'MA0496.2']
-  max1 = ['MA0058.1', 'MA0058.2', 'MA0058.3']
-  mef2a = ['MA0052.1', 'MA0052.2', 'MA0052.3']
-  nfyb = ['MA0502.1', 'MA0060.1', 'MA0060.2']
-  sp1 = ['MA0079.1', 'MA0079.2', 'MA0079.3']
-  srf = ['MA0083.1', 'MA0083.2', 'MA0083.3']
-  stat1 = ['MA0137.1', 'MA0137.2', 'MA0137.3', 'MA0660.1', 'MA0773.1']
-  yy1 = ['MA0095.1', 'MA0095.2']
-
-  motifs = [arid3, cebpb, fosl1, gabpa, mafk, max1, mef2a, nfyb, sp1, srf, stat1, yy1]
-  motif_names = ['Arid3', 'CEBPB', 'FOSL1', 'GABPA', 'MAFK', 'MAX', 'MEF2A', 'NFYB', 'SP1', 'SRF', 'STAT1', 'YY1']
-  match_fraction, match_any, filter_match, filter_qvalue, min_qvalue, num_counts = match_hits_to_ground_truth(file_path, motifs, motif_names, num_filters)
-
-  return match_fraction, match_any, filter_match, filter_qvalue, min_qvalue, num_counts
-
-
-
 
 
