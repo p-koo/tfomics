e9f5135762ac34e19df65971c2ec0d40d8e022e8
koo-lab <koolaboratory.gmail.com>
2021-01-26 17:03:05 -0500

    nn bug in revcompconv1d

--- layers.py
+++ layers.py
@@ -42,9 +42,9 @@ class RevCompConv1D(keras.layers.Conv1D):
 
     # add bias
     if self.use_bias:
-      outputs = tf.nn.bias_add(outputs, self.bias, data_format=self._tf_data_format)
-      rc_outputs = tf.nn.bias_add(rc_outputs, self.bias, data_format=self._tf_data_format)
-
+      outputs = tf.nn.bias_add(outputs, self.bias)
+      rc_outputs = tf.nn.bias_add(rc_outputs, self.bias)
+      
     # add activations
     if self.activation is not None:
       outputs = self.activation(outputs)
