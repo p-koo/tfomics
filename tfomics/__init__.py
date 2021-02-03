b53c28e3deda64bcaac9e71d4115029e871642a3
koo-lab <koolaboratory.gmail.com>
2021-02-03 09:52:47 -0500

    fixed bug in RevCompConv1D super

--- tfomics/__init__.py
+++ tfomics/__init__.py
@@ -1,11 +1,13 @@
 
+from . import attack 
 from . import evaluate 
 from . import explain 
+from . import fit_custom 
 from . import fit 
+from . import impress 
 from . import layers 
 from . import metrics 
 from . import moana 
+from . import model_custom 
+from . import objectives 
 from . import utils 
-from . import attack 
-from . import fit_custom 
-#from . import model_custom 
