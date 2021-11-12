eb2d791dd27184b205d680c2205d8bdb4d023a86
koo-lab <koolaboratory.gmail.com>
2021-11-11 20:26:05 -0500

    updates

--- tfomics/moana.py
+++ tfomics/moana.py
@@ -8,6 +8,101 @@ import subprocess
 #---------------------------------------------------------------------------------------
 # Get position probability matrix of conv filters
 
+
+def filter_max_align_batch(X, model, layer=3, window=24, threshold=0.5, batch_size=1024, max_align=1e4, verbose=1):
+  """get alignment of filter activations for visualization"""
+
+  if verbose:
+    print("Calculating filter PPM based on activation-based alignments")
+  N,L,A = X.shape
+  num_filters = model.layers[layer].output.shape[2]
+
+  # Set the left and right window sizes
+  window_left = int(window/2)
+  window_right = window - window_left
+
+  # get feature maps of 1st convolutional layer after activation
+  intermediate = keras.Model(inputs=model.inputs, outputs=model.layers[layer].output)
+
+  dataset = tf.data.Dataset.from_tensor_slices(X)
+  batches = dataset.batch(batch_size)
+    
+  # loop over batches to capture MAX activation
+  if verbose:
+    print('  Step 1 of 2: calculating MAX activation')
+  MAX = np.zeros(num_filters)
+  for x in batches:
+
+    # get feature map for mini-batch
+    fmap = intermediate.predict(x)
+
+    # loop over each filter to find "active" positions
+    for f in range(num_filters):
+      MAX[f] = np.maximum(MAX[f], np.max(fmap[:,:,f]))        
+        
+        
+  # loop over each filter to find "active" positions
+  if verbose:
+    print('  Step 2 of 2: generating activation-based alignments')
+  W = []
+  counts = []
+  for f in range(num_filters):
+    if verbose:
+      print("    processing filter %d out of %d"%(f+1, num_filters))
+    status = 0
+
+    # compile sub-model to get feature map
+    intermediate = keras.Model(inputs=model.inputs, outputs=model.layers[layer].output[:,:,f])
+        
+    # loop over each batch
+    dataset = tf.data.Dataset.from_tensor_slices(X)
+    seq_align_sum = np.zeros((window, A)) # running sum
+    counter = 0                            # counts the number of sequences in alignment
+    status = 1                            # monitors whether depth of alignment has reached max_align
+    for x in dataset.batch(batch_size):
+      if status:
+        
+        # get feature map for a batch sequences
+        fmaps = intermediate.predict(x)
+
+        # Find regions above threshold
+        for data_index, fmap in enumerate(fmaps):
+          if status:
+            pos_index = np.where(fmap > MAX[f] * threshold)[0]
+
+            # Make a sequence alignment centered about each activation (above threshold)
+            for i in range(len(pos_index)):
+              if status:
+                # Determine position of window about each filter activation
+                start_window = pos_index[i] - window_left
+                end_window = pos_index[i] + window_right
+
+                # Check to make sure positions are valid
+                if (start_window > 0) & (end_window < L):
+                  seq_align_sum += x[data_index,start_window:end_window,:].numpy()
+                  counter += 1
+                  if counter > max_align:
+                    status = 0
+                else:
+                  break
+          else:
+            break
+      else: 
+        if verbose:
+          print("      alignment has reached max depth for all filters")
+        break
+    
+    # calculate position probability matrix of filter
+    if verbose:
+      print("      %d sub-sequences in alignment"%(counter))
+    if counter > 0:
+      W.append(seq_align_sum/counter)
+    else:
+      W.append(np.ones((window,A))/A)
+    counts.append(counter)
+  return np.array(W), np.array(counts)
+
+
 def filter_activations(x_test, model, layer=3, window=20, threshold=0.5):
   """get alignment of filter activations for visualization"""
 
